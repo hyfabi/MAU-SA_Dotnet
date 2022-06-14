18ba6a255d1c4aa5962b6ed4afbdace1c93c8bc4
Edmond Sahiti <sah19009@spengergasse.at>
2022-06-14 14:23:03 +0200

    Enum start

--- at.mausa.grasmaster.domain/Models/Product.cs
+++ at.mausa.grasmaster.domain/Models/Product.cs
@@ -1,9 +1,16 @@
 ﻿namespace At.Mausa.Grasmaster.Domain.Models {
+
+    public enum ProductType 
+    {
+        Grashalm, Blätter, Gras_von_Boden
+    }
+
     public class Product : Entity {
         public Product(Guid? guid) : base(guid) { }
 
         public string Description { get; set; } = "";
         public string Name { get; set; } = "";
+        public ProductType Type { get; set; }
 
         public void UpdateName(string name) {
             if(string.IsNullOrEmpty(name?.Trim()))
